71a7d4f29b92171d07fae39640ad9446638af9e8
sayimpuvinay <vinaysayimpu@gmail.com>
2023-05-22 17:39:47 +0530

    github trail

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import 'bulma/css/bulma.css';
 import ProfileCard from './ProfileCard';
 import AlexaImage from './images/alexa.png';
 import CortanaImage from './images/cortana.png';
